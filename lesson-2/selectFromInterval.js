21cc0d1be566df49361c240e98a7df1da38332b7
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-04-24 19:09:35 +0300

    refractor

--- lesson-2/selectFromInterval.js
+++ lesson-2/selectFromInterval.js
@@ -1,18 +1,15 @@
 module.exports = function selectFromInterval(arr, start, end) {
 	if (!arr || !start || !end || !Array.isArray(arr)) throwErr();
-	let result;
 	if (start > end) {
-		result = arr.filter((item) =>
+		return arr.filter((item) =>
 			typeof item != 'number' ? throwErr() : start >= item && item >= end,
 		);
 	} else if (start < end) {
-		result = arr.filter((item) =>
+		return arr.filter((item) =>
 			typeof item != 'number' ? throwErr() : start <= item && item <= end,
 		);
 	}
 	function throwErr() {
 		throw new Error('Ошибка!');
 	}
-
-	return result;
 };
