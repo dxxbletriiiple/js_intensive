11fec647a5f12d6fda071d8af063a365efcdf7cd
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-04-24 08:13:26 +0300

    refractor

--- lesson-2/copyObj.js
+++ lesson-2/copyObj.js
@@ -1,14 +1,7 @@
 module.exports = function makeObjectDeepCopy(obj) {
 	let result = {};
 	for (let key in obj) {
-		if (typeof key == 'object' && obj[key] != null) {
-			if (Array.isArray(key)) {
-				key.forEach((element) => {
-					if (typeof element == 'object') makeObjectDeepCopy(element);
-				});
-			}
-			makeObjectDeepCopy(key);
-		}
+		if (typeof key == 'object' && obj[key] != null) makeObjectDeepCopy(key);
 		result[key] = obj[key];
 	}
 	return result;
