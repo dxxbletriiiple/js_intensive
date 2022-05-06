16e58fd676d17817caf858485be492331ab514d2
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-05-06 20:00:39 +0300

    calculator: done

---
+++ lesson-5/calc.js
@@ -0,0 +1,33 @@
+class Calculator {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	set setX(newX) {
+		if (newX == undefined || typeof newX != 'number') throw new Error('Invalid first argument');
+		this.x = newX;
+	}
+	get setX() {
+		return this.x;
+	}
+	set setY(newY) {
+		if (newY == undefined || typeof newY != 'number')
+			throw new Error('Invalid second argument');
+		this.y = newY;
+	}
+
+	logSum() {
+		return console.log(this.x + this.y);
+	}
+	logSub() {
+		return console.log(this.x - this.y);
+	}
+	logMul() {
+		return console.log(this.x * this.y);
+	}
+	logDiv() {
+		return console.log(this.x / this.y);
+	}
+}
+
+const calc = new Calculator(1, 1);
